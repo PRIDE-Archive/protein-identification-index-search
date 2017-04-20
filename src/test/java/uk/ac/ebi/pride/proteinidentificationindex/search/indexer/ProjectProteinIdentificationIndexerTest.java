9603b9f70417af53ab04f3d8cca97f72622bf2f0
Tobias-Ternent <tobias@ebi.ac.uk>
2017-04-20 08:47:53 +0100

    Improved unit test with basic counts.

--- src/test/java/uk/ac/ebi/pride/proteinidentificationindex/search/indexer/ProjectProteinIdentificationIndexerTest.java
+++ src/test/java/uk/ac/ebi/pride/proteinidentificationindex/search/indexer/ProjectProteinIdentificationIndexerTest.java
@@ -29,76 +29,78 @@ import java.util.List;
 
 public class ProjectProteinIdentificationIndexerTest extends SolrTestCaseJ4 {
 
-    private static Logger logger = LoggerFactory.getLogger(ProjectProteinIdentificationIndexerTest.class.getName());
 
-    private static final String TEST_SUBMITTED_SEQ = "MSSEEVVVAVEEQEIPDVIERLMSSEEVVVAVEEQEIPDVIERLMSSEEVVVAVEEQEIPDVIERL";
-    private static final String TEST_ID = "TEST_ID";
-    private static final String TEST_SUBMITTED_AC = "D0NNB3";
+  private static Logger logger = LoggerFactory.getLogger(ProjectProteinIdentificationIndexerTest.class.getName());
 
-    private static final String TEST_PROTEIN_ACCESSION = "D0NNB3";
-    private static SolrServer server;
+  private static final String TEST_PROJ_ACCESSION = "PXT000001";
+  private static final String TEST_ASSAY_ACCESSION = "123456";
+  private static final String TEST_ID = "TEST_ID";
 
-    private static SolrProteinIdentificationRepositoryFactory solrProteinIdentificationRepositoryFactory;
+  private static final String TEST_PROTEIN_ACCESSION = "D0NNB3";
+  private static SolrServer server;
 
-    public static final long ZERO_DOCS = 0L;
+  private static SolrProteinIdentificationRepositoryFactory solrProteinIdentificationRepositoryFactory;
 
-    @BeforeClass
-    public static void initialise() throws Exception {
-        initCore("src/test/resources/solr/protein-identification-index/conf/solrconfig.xml",
-                "src/test/resources/solr/protein-identification-index/conf/schema.xml",
-                "src/test/resources/solr",
-                "protein-identification-index");
-    }
+  public static final long ZERO_DOCS = 0L;
 
-    @AfterClass
-    public static void cleanUp() throws Exception {
-        deleteCore();
-    }
+  @BeforeClass
+  public static void initialise() throws Exception {
+    initCore("src/test/resources/solr/protein-identification-index/conf/solrconfig.xml",
+        "src/test/resources/solr/protein-identification-index/conf/schema.xml",
+        "src/test/resources/solr",
+        "protein-identification-index");
+  }
 
-
-    @Before
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-        server = new EmbeddedSolrServer(h.getCoreContainer(), h.getCore().getName());
-        server.deleteByQuery("*:*");
-        solrProteinIdentificationRepositoryFactory = new SolrProteinIdentificationRepositoryFactory(new SolrTemplate(server));
-    }
-
-    @Test
-    public void testThatNoResultsAreReturned() throws SolrServerException {
-        SolrParams params = new SolrQuery("text that is not found");
-        QueryResponse response = server.query(params);
-        assertEquals(ZERO_DOCS, response.getResults().getNumFound());
-    }
+  @AfterClass
+  public static void cleanUp() throws Exception {
+    deleteCore();
+  }
 
 
-    @Test
-    public void testAddProtein() {
-        addD0NNb3();
+  @Before
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    server = new EmbeddedSolrServer(h.getCoreContainer(), h.getCore().getName());
+    server.deleteByQuery("*:*");
+    solrProteinIdentificationRepositoryFactory = new SolrProteinIdentificationRepositoryFactory(new SolrTemplate(server));
+  }
 
-        ProteinIdentificationSearchService proteinIdentificationSearchService = new ProteinIdentificationSearchService(solrProteinIdentificationRepositoryFactory.create());
-        ProteinIdentificationIndexService proteinIdentificationIndexService = new ProteinIdentificationIndexService(server, solrProteinIdentificationRepositoryFactory.create());
+  @Test
+  public void testThatNoResultsAreReturned() throws SolrServerException {
+    SolrParams params = new SolrQuery("text that is not found");
+    QueryResponse response = server.query(params);
+    assertEquals(ZERO_DOCS, response.getResults().getNumFound());
+  }
 
-        List<ProteinIdentification> proteins = proteinIdentificationSearchService.findByAccession(TEST_PROTEIN_ACCESSION);
+  @Test
+  public void testAddProtein() {
+    addD0NNb3();
+    ProteinIdentificationSearchService proteinIdentificationSearchService = new ProteinIdentificationSearchService(solrProteinIdentificationRepositoryFactory.create());
+    ProteinIdentificationIndexService proteinIdentificationIndexService = new ProteinIdentificationIndexService(server, solrProteinIdentificationRepositoryFactory.create());
+    List<ProteinIdentification> proteins = proteinIdentificationSearchService.findByAccession(TEST_PROTEIN_ACCESSION);
+    proteinIdentificationIndexService.save(proteins.get(0));
+    proteins = proteinIdentificationSearchService.findByAccession(TEST_PROTEIN_ACCESSION);
+    assertNotNull(proteins);
+    assertNotNull(proteins.get(0));
+    assertEquals(proteins.get(0).getId(), TEST_ID);
+    assertEquals(proteins.get(0).getAccession(), TEST_PROTEIN_ACCESSION);
+    Long count = proteinIdentificationSearchService.countByAssayAccession(TEST_ASSAY_ACCESSION);
+    assertEquals(count.longValue(), 1L);
+    count = proteinIdentificationSearchService.countByProjectAccession(TEST_PROJ_ACCESSION);
+    assertEquals(count.longValue(), 1L);
+    count = proteinIdentificationSearchService.countByProjectAccessionAndAccession(TEST_PROJ_ACCESSION, TEST_PROTEIN_ACCESSION);
+    assertEquals(count.longValue(), 1L);
+  }
 
-        proteinIdentificationIndexService.save(proteins.get(0));
 
-        proteins = proteinIdentificationSearchService.findByAccession(TEST_PROTEIN_ACCESSION);
-
-        assertNotNull(proteins);
-        assertNotNull(proteins.get(0));
-        assertEquals(proteins.get(0).getId(), TEST_ID);
-        assertEquals(proteins.get(0).getAccession(), TEST_PROTEIN_ACCESSION);
-
-    }
-
-
-    private void addD0NNb3() {
-        ProteinIdentificationIndexService proteinIdentificationIndexService = new ProteinIdentificationIndexService(server, solrProteinIdentificationRepositoryFactory.create());
-        ProteinIdentification proteinIdentification = new ProteinIdentification();
-        proteinIdentification.setId(TEST_ID);
-        proteinIdentification.setAccession(TEST_PROTEIN_ACCESSION);
-        proteinIdentificationIndexService.save(proteinIdentification);
-    }
+  private void addD0NNb3() {
+    ProteinIdentificationIndexService proteinIdentificationIndexService = new ProteinIdentificationIndexService(server, solrProteinIdentificationRepositoryFactory.create());
+    ProteinIdentification proteinIdentification = new ProteinIdentification();
+    proteinIdentification.setId(TEST_ID);
+    proteinIdentification.setProjectAccession(TEST_PROJ_ACCESSION);
+    proteinIdentification.setAssayAccession(TEST_ASSAY_ACCESSION);
+    proteinIdentification.setAccession(TEST_PROTEIN_ACCESSION);
+    proteinIdentificationIndexService.save(proteinIdentification);
+  }
 }
